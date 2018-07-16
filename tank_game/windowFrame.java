939c0aa41800382a9672b6526b293106ac66b9d9
Alexander Caley <zanner11030@gmail.com>
2018-07-15 19:34:54 -0700

    testing

--- tank_game/windowFrame.java
+++ tank_game/windowFrame.java
@@ -8,10 +8,9 @@ public class windowFrame extends Canvas {
     public windowFrame(String title, int width, int height, tankGame game){
         JFrame frame = new JFrame(title);
         frame.setSize(width, height);
-        frame.add(game);
-        frame.pack();
-        frame.setVisible(true);
         frame.setResizable(false);
+        frame.add(game);
+        frame.setVisible(true);
         game.start();
     }
 }
